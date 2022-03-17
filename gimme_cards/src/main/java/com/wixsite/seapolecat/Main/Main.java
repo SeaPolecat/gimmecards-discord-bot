3ba1739f940b4bc720c9dc210da6981e8cafd637
Wayne Wang <wangwayne400@gmail.com>
2022-03-17 15:38:35 -0700

    restructured project and added Fusion Strike

---
+++ gimme_cards/src/main/java/com/wixsite/seapolecat/Main/Main.java
@@ -0,0 +1,50 @@
+package com.wixsite.seapolecat.Main;
+import com.wixsite.seapolecat.Cmds.*;
+import com.wixsite.seapolecat.Display.*;
+import javax.security.auth.login.LoginException;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import org.discordbots.api.client.DiscordBotListAPI;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Main {
+
+    public static JDA jda;
+    public static DiscordBotListAPI dbl;
+
+    public static void main(String[] args) throws LoginException {
+
+        //Dotenv dotenv = Dotenv.load();
+        String botToken = "ODY3MTA1NjEzNzIwNTg0MjIy.YPcRCA.Xtytp7Dh2-v3xb1ch1J92DPqUcw";
+        String dblToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgxNDAyNTQ5OTM4MTcyNzIzMiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjQ3NDU5MTA0fQ.LSG6P0ltDzOtMhlMBTdhJyQ7ZstH0n9PR_TLf3gS3Ik";
+        String botId = "814025499381727232";
+
+        jda = JDABuilder
+        .createDefault(botToken,
+        GatewayIntent.GUILD_MESSAGES,
+        GatewayIntent.GUILD_MESSAGE_REACTIONS,
+        GatewayIntent.GUILD_EMOJIS,
+        GatewayIntent.GUILD_MEMBERS)
+        .setChunkingFilter(ChunkingFilter.ALL)
+        .enableCache(CacheFlag.EMOTE)
+        .disableCache(CacheFlag.VOICE_STATE)
+        .build();
+
+        jda.getPresence().setStatus(OnlineStatus.ONLINE);
+        jda.getPresence().setActivity(Activity.playing("?help"));
+
+        jda.addEventListener(new Ready());
+        jda.addEventListener(new Cmds());
+        jda.addEventListener(new Display());
+
+        dbl = new DiscordBotListAPI.Builder()
+        .token(dblToken)
+        .botId(botId)
+        .build();
+    }
+}
