1bacf32ab8de75ec1aa5c2b42e780a93cb136317
SeaPolecat <wangwayne400@gmail.com>
2023-08-20 01:44:40 -0700

    oops forgot something
    hid the tokens and included the storage files

--- src/main/java/ca/gimmecards/Main/Main.java
+++ src/main/java/ca/gimmecards/Main/Main.java
@@ -25,10 +25,10 @@ public class Main {
 
     // PLEASE REDACT THESE BEFORE UPLOADING TO GITHUB!!!!!!
     
-    public static final String botToken = "ODE0MDI1NDk5MzgxNzI3MjMy.GuoICs.Hf2yW37yztgjcDa6Ov4mJy6kn2lcN0DhEgG4Dg";
-    public static final String testToken = "ODY3MTA1NjEzNzIwNTg0MjIy.G6YPjJ.ne63D-J9CF_swKCP4N6kj9wRLQ6bqC1K_8rtPg";
-    public static final String dblToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgxNDAyNTQ5OTM4MTcyNzIzMiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjUzMjA1MjI3fQ.cKoI_mWYtI6WeJ4boujB3zW6QVqe8Fl1YlnoAj57fMw";
-    public static final String encryptorPass = "uxdfINYH8jS4QILwTLub6HZZz9r2GPstOI5Jh9cWTwsm09fQ7S";
+    public static final String botToken = "";
+    public static final String testToken = "";
+    public static final String dblToken = "";
+    public static final String encryptorPass = "";
 
     //========================================[ UNIVERSAL VARIABLES ]==================================================================
 
