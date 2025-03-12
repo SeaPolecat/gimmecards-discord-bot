e5a8b6fab0d0f4d80d3721c9afca836ca9afb206
SeaPolecat <wangwayne400@gmail.com>
2025-03-11 20:43:21 -0400

    made searchedCards an instance var, and performed the search in the SearchCmds class instead, so the data is only crawled when the cmd is invoked, and not every time someone flips a page

--- src/main/java/ca/gimmecards/Cmds/SearchCmds.java
+++ src/main/java/ca/gimmecards/Cmds/SearchCmds.java
@@ -5,6 +5,7 @@ import ca.gimmecards.main.*;
 import ca.gimmecards.utils.*;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import java.util.ArrayList;
 
 public class SearchCmds {
 
@@ -28,6 +29,8 @@ public class SearchCmds {
                     isExact.getAsBoolean(),
                     keywords.getAsString()
                 );
+                disp.setSearchedCards(searchCollection(user, disp));
+
                 JDAUtils.sendDynamicEmbed(event, disp, user, null, true);
             }
 
@@ -38,6 +41,8 @@ public class SearchCmds {
                 isExact.getAsBoolean(),
                 keywords.getAsString()
             );
+            disp.setSearchedCards(searchPokedex(user, disp));
+
             JDAUtils.sendDynamicEmbed(event, disp, user, null, true);
         }
     }
@@ -55,6 +60,111 @@ public class SearchCmds {
 
         } catch(NullPointerException e) {
             JDAUtils.sendMessage(event, ColorConsts.RED, "❌", "Whoops, I couldn't find that card...");
+        }
+    }
+
+    private static String findName(Card card, SearchDisplay disp) {
+        if(disp.getFilter().equalsIgnoreCase("card")) {
+            return card.getCardName();
+
+        } else if(disp.getFilter().equalsIgnoreCase("pack")) {
+            return card.getSetName();
+        }
+        return card.getCardRarity();
+    }
+
+    private static ArrayList<Card> searchCollection(User user, SearchDisplay disp) {
+        ArrayList<Card> searchedCards = new ArrayList<Card>();
+
+        for(CardContainer cc : user.getCardContainers()) {
+            Card card = cc.getCard();
+            String name = findName(card, disp);
+            boolean isExact = disp.getIsExact();
+            String keywords = disp.getKeywords();
+
+            if(isExact ? name.equalsIgnoreCase(keywords) : name.toLowerCase().contains(keywords.toLowerCase())) {
+                searchedCards.add(card);
+            }
+        }
+        return searchedCards;
+    }
+
+    private static ArrayList<Card> searchPokedex(User user, SearchDisplay disp) {
+        ArrayList<Card> searchedCards = new ArrayList<Card>();
+
+        for(CardSet set : CardSet.sets) {
+            crawlSet(searchedCards, set, disp);
+        }
+        for(CardSet oldSet : CardSet.oldSets) {
+            crawlSet(searchedCards, oldSet, disp);
+        }
+        crawlSpecialSets(searchedCards, disp);
+
+        return searchedCards;
+    }
+
+    private static void crawlSet(ArrayList<Card> searchedCards, CardSet set, SearchDisplay disp) {
+        boolean isExact = disp.getIsExact();
+        String keywords = disp.getKeywords();
+
+        for(Card card : set.getCommons()) {
+            String name = findName(card, disp);
+            
+            if(isExact ? name.equalsIgnoreCase(disp.getKeywords()) : name.toLowerCase().contains(keywords.toLowerCase())) {
+                searchedCards.add(card);
+            }
+        }
+        for(Card card : set.getUncommons()) {
+            String name = findName(card, disp);
+
+            if(isExact ? name.equalsIgnoreCase(disp.getKeywords()) : name.toLowerCase().contains(keywords.toLowerCase())) {
+                searchedCards.add(card);
+            }
+        }
+        for(Card card : set.getRares()) {
+            String name = findName(card, disp);
+
+            if(isExact ? name.equalsIgnoreCase(disp.getKeywords()) : name.toLowerCase().contains(keywords.toLowerCase())) {
+                searchedCards.add(card);
+            }
+        }
+        for(Card card : set.getShinies()) {
+            String name = findName(card, disp);
+
+            if(isExact ? name.equalsIgnoreCase(disp.getKeywords()) : name.toLowerCase().contains(keywords.toLowerCase())) {
+                searchedCards.add(card);
+            }
+        }
+    }
+
+    private static void crawlSpecialSets(ArrayList<Card> searchedCards, SearchDisplay disp) {
+        boolean isExact = disp.getIsExact();
+        String keywords = disp.getKeywords();
+
+        for(CardSet specSet : CardSet.rareSets) {
+            for(Card card : specSet.getSpecials()) {
+                String name = findName(card, disp);
+
+                if(isExact ? name.equalsIgnoreCase(keywords) : name.toLowerCase().contains(keywords.toLowerCase())) {
+                    searchedCards.add(card);
+                }
+            }
+        }
+        for(CardSet specSet : CardSet.promoSets) {
+            for(Card card : specSet.getSpecials()) {
+                String name = findName(card, disp);
+
+                if(isExact ? name.equalsIgnoreCase(keywords) : name.toLowerCase().contains(keywords.toLowerCase())) {
+                    searchedCards.add(card);
+                }
+            }
+        }
+        for(Card card : CustomCardConsts.CUSTOM_CARDS) {
+            String name = findName(card, disp);
+
+            if(isExact ? name.equalsIgnoreCase(keywords) : name.toLowerCase().contains(keywords.toLowerCase())) {
+                searchedCards.add(card);
+            }
         }
     }
 }
--- src/main/java/ca/gimmecards/Display/SearchDisplay.java
+++ src/main/java/ca/gimmecards/Display/SearchDisplay.java
@@ -12,6 +12,7 @@ public class SearchDisplay extends Display {
     private String filter;
     private boolean isExact;
     private String keywords;
+    ArrayList<Card> searchedCards; 
 
     public SearchDisplay(SlashCommandInteractionEvent event) {
         super(event);
@@ -19,7 +20,7 @@ public class SearchDisplay extends Display {
 
     public String getLocation() { return this.location; }
     public String getFilter() { return this.filter; }
-    public boolean isExact() { return this.isExact; }
+    public boolean getIsExact() { return this.isExact; }
     public String getKeywords() { return this.keywords; }
 
     public void modifyVariables(String location, String filter, boolean isExact, String keywords) {
@@ -28,105 +29,9 @@ public class SearchDisplay extends Display {
         this.isExact = isExact;
         this.keywords = keywords;
     }
+    public void setSearchedCards(ArrayList<Card> searchedCards) { this.searchedCards = searchedCards; }
 
-    public String findName(Card card) {
-        if(this.filter.equalsIgnoreCase("card")) {
-            return card.getCardName();
-
-        } else if(this.filter.equalsIgnoreCase("pack")) {
-            return card.getSetName();
-        }
-        return card.getCardRarity();
-    }
-
-    public ArrayList<Card> searchCollection(User user) {
-        ArrayList<Card> searchedCards = new ArrayList<Card>();
-
-        for(CardContainer cc : user.getCardContainers()) {
-            Card card = cc.getCard();
-            String name = findName(card);
-
-            if(this.isExact ? name.equalsIgnoreCase(this.keywords) : name.toLowerCase().contains(this.keywords.toLowerCase())) {
-                searchedCards.add(card);
-            }
-        }
-        return searchedCards;
-    }
-
-    public ArrayList<Card> searchPokedex(User user) {
-        ArrayList<Card> searchedCards = new ArrayList<Card>();
-
-        for(CardSet set : CardSet.sets) {
-            crawlSet(searchedCards, set);
-        }
-        for(CardSet oldSet : CardSet.oldSets) {
-            crawlSet(searchedCards, oldSet);
-        }
-        crawlSpecialSets(searchedCards);
-
-        return searchedCards;
-    }
-
-    public void crawlSet(ArrayList<Card> searchedCards, CardSet set) {
-        for(Card card : set.getCommons()) {
-            String name = findName(card);
-            
-            if(this.isExact ? name.equalsIgnoreCase(this.keywords) : name.toLowerCase().contains(this.keywords.toLowerCase())) {
-                searchedCards.add(card);
-            }
-        }
-        for(Card card : set.getUncommons()) {
-            String name = findName(card);
-
-            if(this.isExact ? name.equalsIgnoreCase(this.keywords) : name.toLowerCase().contains(this.keywords.toLowerCase())) {
-                searchedCards.add(card);
-            }
-        }
-        for(Card card : set.getRares()) {
-            String name = findName(card);
-
-            if(this.isExact ? name.equalsIgnoreCase(this.keywords) : name.toLowerCase().contains(this.keywords.toLowerCase())) {
-                searchedCards.add(card);
-            }
-        }
-        for(Card card : set.getShinies()) {
-            String name = findName(card);
-
-            if(this.isExact ? name.equalsIgnoreCase(this.keywords) : name.toLowerCase().contains(this.keywords.toLowerCase())) {
-                searchedCards.add(card);
-            }
-        }
-    }
-
-    public void crawlSpecialSets(ArrayList<Card> searchedCards) {
-        for(CardSet specSet : CardSet.rareSets) {
-            for(Card card : specSet.getSpecials()) {
-                String name = findName(card);
-
-                if(this.isExact ? name.equalsIgnoreCase(this.keywords) : name.toLowerCase().contains(this.keywords.toLowerCase())) {
-                    searchedCards.add(card);
-                }
-            }
-        }
-        for(CardSet specSet : CardSet.promoSets) {
-            for(Card card : specSet.getSpecials()) {
-                String name = findName(card);
-
-                if(this.isExact ? name.equalsIgnoreCase(this.keywords) : name.toLowerCase().contains(this.keywords.toLowerCase())) {
-                    searchedCards.add(card);
-                }
-            }
-        }
-        for(Card card : CustomCardConsts.CUSTOM_CARDS) {
-            String name = findName(card);
-
-            if(this.isExact ? name.equalsIgnoreCase(this.keywords) : name.toLowerCase().contains(this.keywords.toLowerCase())) {
-                searchedCards.add(card);
-            }
-        }
-    }
-
-    private void getValidPage(ArrayList<Card> searchedCards) {
+    private void getValidPage() {
         int numFullPages = searchedCards.size() / 15;
         int remainder = searchedCards.size() % 15;
 
@@ -134,8 +39,6 @@ public class SearchDisplay extends Display {
             setMaxPage(numFullPages);
         else
             setMaxPage(numFullPages + (remainder / remainder));
-            
-        checkOverflow();
     }
 
     @Override
@@ -145,79 +48,63 @@ public class SearchDisplay extends Display {
         EmbedBuilder embed = new EmbedBuilder();
         String desc = "";
 
-        if(this.location.equalsIgnoreCase("collection")) {
-            ArrayList<Card> searchedCards = searchCollection(user);
+        getValidPage();
 
-            getValidPage(searchedCards);
+        if(this.location.equalsIgnoreCase("collection")) {
 
             desc += "***" + FormatUtils.formatNumber(searchedCards.size()) + "*** ";
             desc += this.isExact ? "exact " : "non-exact ";
             desc += "search results for `" + this.keywords + "`\n\n";
             desc += "`/view (card #)` to view a card\n";
+            desc += "┅┅\n";
 
-            if(searchedCards.size() > 0) {
-                desc += "┅┅\n";
+            for(int i = startIndex; i < startIndex + 15; i++) {
+                Card searchedCard = searchedCards.get(i);
 
-                for(int i = startIndex; i < startIndex + 15; i++) {
-                    Card searchedCard = searchedCards.get(i);
-
-                    for(int k = 0; k < user.getCardContainers().size(); k++) {
-                        CardContainer cc = user.getCardContainers().get(k);
-                        Card card = user.getCardContainers().get(k).getCard();
-                        int cardNum = k + 1;
-                        
-                        if(searchedCard.getCardId().equalsIgnoreCase(card.getCardId())) {
-                            desc += "`#" + cardNum + "` " + card.findCardTitle(cc.getIsFav())
-                            + " ┇ " + card.findRarityEmote() 
-                            + " ┇ " + card.getSetEmote()
-                            + " ┇ " + card.formatXP(cc.getIsSellable())
-                            + " ┇ *x" + cc.getCardQuantity() + "*\n";
-                        }
-                    }
-                    if(i >= searchedCards.size() - 1) {
-                        break;
+                for(int k = 0; k < user.getCardContainers().size(); k++) {
+                    CardContainer cc = user.getCardContainers().get(k);
+                    Card card = user.getCardContainers().get(k).getCard();
+                    int cardNum = k + 1;
+                    
+                    if(searchedCard.getCardId().equalsIgnoreCase(card.getCardId())) {
+                        desc += "`#" + cardNum + "` " + card.findCardTitle(cc.getIsFav())
+                        + " ┇ " + card.findRarityEmote() 
+                        + " ┇ " + card.getSetEmote()
+                        + " ┇ " + card.formatXP(cc.getIsSellable())
+                        + " ┇ *x" + cc.getCardQuantity() + "*\n";
                     }
                 }
-                desc += "┅┅\n";
-
-            } else {
-                setMaxPage(1);
+                if(i >= searchedCards.size() - 1) {
+                    break;
+                }
             }
+            desc += "┅┅\n";
             embed.setTitle(EmoteConsts.POKEBALL + " Searching " + ui.getUserName() + "'s Collection... " + EmoteConsts.POKEBALL);
             embed.setDescription(desc);
             embed.setFooter("Page " + getPage() + " of " + getMaxPage(), ui.getUserIcon());
             embed.setColor(ColorConsts.SEARCH_COLOR);
 
         } else if(this.location.equals("pokedex")) {
-            ArrayList<Card> searchedCards = searchPokedex(user);
-
-            getValidPage(searchedCards);
 
             desc += "***" + FormatUtils.formatNumber(searchedCards.size()) + "*** ";
             desc += this.isExact ? "exact " : "non-exact ";
             desc += "search results for `" + this.keywords + "`\n\n";
             desc += "`/sview (card ID)` to view a card\n";
+            desc += "┅┅\n";
 
-            if(searchedCards.size() > 0) {
-                desc += "┅┅\n";
+            for(int i = startIndex; i < startIndex + 15; i++) {
+                Card searchedCard = searchedCards.get(i);
 
-                for(int i = startIndex; i < startIndex + 15; i++) {
-                    Card searchedCard = searchedCards.get(i);
-
-                    desc += searchedCard.findCardTitle(false)
-                    + " ┇ " + searchedCard.findRarityEmote()
-                    + " ┇ " + searchedCard.getSetEmote()
-                    + " ┇ " + searchedCard.formatXP(searchedCard.isCardSellable())
-                    + " ┇ `" + searchedCard.getCardId() + "`\n";
-                    if(i >= searchedCards.size() - 1) {
-                        break;
-                    }
+                desc += searchedCard.findCardTitle(false)
+                + " ┇ " + searchedCard.findRarityEmote()
+                + " ┇ " + searchedCard.getSetEmote()
+                + " ┇ " + searchedCard.formatXP(searchedCard.isCardSellable())
+                + " ┇ `" + searchedCard.getCardId() + "`\n";
+                if(i >= searchedCards.size() - 1) {
+                    break;
                 }
-                desc += "┅┅\n";
-                
-            } else {
-                setMaxPage(1);
             }
+            desc += "┅┅\n";
             embed.setTitle(EmoteConsts.POKEBALL + " Searching the Pokédex... " + EmoteConsts.POKEBALL);
             embed.setDescription(desc);
             embed.setFooter("Page " + getPage() + " of " + getMaxPage(), ui.getUserIcon());
